c793c7e58c636b17d5403ff241e90450661e17e9
webben <wspwebben@gmail.com>
2018-09-08 03:34:05 +0700

    css Header

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var server = require('browser-sync').create();
 
-gulp.task('style', function() {
+gulp.task('build', function() {
   return gulp.src("source/sass/style.scss")    
 		.pipe(sass())
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
-gulp.task('serve', ['style'], function() {
+gulp.task('serve', ['build'], function() {
   server.init({
     server: "source/",
     notify: false,
@@ -18,6 +18,6 @@ gulp.task('serve', ['style'], function() {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", ['style']);
+  gulp.watch("source/sass/**/*.{scss,sass}", ['build']);
   gulp.watch("source/*.html").on('change', server.reload);
 });
